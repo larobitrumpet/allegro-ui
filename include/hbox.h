4b7c5d8d7a2c4888ea379c53d6113cc4d80ddca7
Lucy Robillard <larobitrumpet@lucyrobillard.xyz>
2024-05-02 19:11:45 -0700

    Fix new hbox function name

--- include/hbox.h
+++ include/hbox.h
@@ -4,7 +4,7 @@
 #include "widget.h"
 #include "vector.h"
 
-ALLEGRO_UI_WIDGET* new_hbox();
+ALLEGRO_UI_WIDGET* al_ui_new_hbox();
 void al_ui_hbox_render(ALLEGRO_UI_WIDGET* hbox, float x, float y, float width, float height);
 void al_ui_destroy_hbox(ALLEGRO_UI_WIDGET* hbox);
 float al_ui_hbox_get_width(ALLEGRO_UI_WIDGET* hbox, float width);
